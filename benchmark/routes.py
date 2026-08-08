0b59c08fe2bd984a3bbcc7121d41ddc17d53538b
aliash01 <alirezashkboos@gmail.com>
2026-08-08 23:44:08 +0100

    Initial Commit

---
+++ benchmark/routes.py
@@ -0,0 +1,7 @@
+ROUTES = [
+    {"id": 0, "start": 0, "end": 9},
+    {"id": 1, "start": 0, "end": 13},
+    {"id": 2, "start": 0, "end": 42},
+    {"id": 3, "start": 0, "end": 109},
+    {"id": 4, "start": 0, "end": 57},
+]
---
+++ scripts/explore_routes.py
@@ -0,0 +1,22 @@
+# "C:\UnrealEngine5_carla\Engine\Binaries\Win64\UnrealEditor.exe" "C:\CarlaUE5\Unreal\CarlaUnreal\CarlaUnreal.uproject" -game -windowed -ResX=1280 -ResY=720
+
+import sys
+sys.path.insert(0, r'C:\CarlaUE5\PythonAPI\carla')
+import carla
+from agents.navigation.global_route_planner import GlobalRoutePlanner
+from collections import Counter
+
+client = carla.Client('localhost', 2000)
+client.set_timeout(10.0)
+world = client.get_world()
+
+town_map = world.get_map()
+spawnpoint = town_map.get_spawn_points()[0] 
+
+grp = GlobalRoutePlanner(town_map, sampling_resolution=2.0)
+
+for i, dest in enumerate(town_map.get_spawn_points()):
+    distance_from_spawn = spawnpoint.location.distance(dest.location)
+    if distance_from_spawn > 20.0:
+        route = grp.trace_route(spawnpoint.location, dest.location)
+        print(i, distance_from_spawn, len(route) * 2, Counter(opt.name for _, opt in route))
